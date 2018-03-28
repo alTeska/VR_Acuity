8ceb2100366b378e48cf166b382484e1231fac12
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-03-28 14:14:39 +0200

    restruct

--- __init__.py
+++
@@ -1,4 +0,0 @@
-import relationalDatabaseRat     as rDR
-import relationalDatabaseEvent   as rDE
-import relationalDatabaseSession as rDS
-import h5py
---
+++ build.sh
@@ -0,0 +1,5 @@
+python scripts/relationalDatabaseRat.py
+python scripts/relationalDatabaseEvent.py
+python scripts/relationalDatabaseSession.py
+
+python scripts/relationalDatabase.py
