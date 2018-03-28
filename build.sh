0beb6f7c949f7e6e872f57b5f296d26c45ae27aa
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-03-28 14:23:21 +0200

    html + bash

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ python scripts/relationalDatabaseEvent.py
 python scripts/relationalDatabaseSession.py
 
 python scripts/relationalDatabase.py
+
+jupyter nbconvert --to html reports/*.ipynb --output-dir reports/html
